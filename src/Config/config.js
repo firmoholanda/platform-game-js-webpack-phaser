cb839c52862009ee51545e288cbd4ea258d3122c
firmo holanda <firmo@viize.com>
2020-07-10 18:06:24 -0300

    create level01 for game scene

--- src/Config/config.js
+++ src/Config/config.js
@@ -2,7 +2,14 @@ import 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  parent: 'game',
   width: 800,
-  height: 600
+  height: 640,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 500 },
+      debug: false,
+    },
+  },
 };
