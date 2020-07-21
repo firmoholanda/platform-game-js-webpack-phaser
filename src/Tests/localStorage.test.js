7aba49d967b4ae72b29faecbff3b1a79c28d82e2
firmo holanda <firmo@viize.com>
2020-07-21 13:08:30 -0300

    add more leaderboardAPI.test and localStorage.test tests

--- src/Tests/localStorage.test.js
+++ src/Tests/localStorage.test.js
@@ -1,10 +1,8 @@
-// import { initSaveLocal, getLocalName, getLocalScore } from '../Helpers/localStorage';
-
 const localStorageTest = require('../Helpers/localStorage');
 
 localStorageTest.initSaveLocal();
 
-describe('test getLocalName', () => {
+describe('test getLocalName from init', () => {
   const name = localStorageTest.getLocalName();
 
   test('should return name anonymous', () => {
@@ -12,10 +10,28 @@ describe('test getLocalName', () => {
   });
 });
 
-describe('test getLocalScore', () => {
+describe('test getLocalScore from init', () => {
   const score = localStorageTest.getLocalScore();
 
   test('should return score 0', () => {
     expect(score).toBe(0);
+  });
+});
+
+describe('test getLocalName form user imput', () => {
+  localStorageTest.saveLocalName('anne donne');
+  const name = localStorageTest.getLocalName();
+
+  test('should return name', () => {
+    expect(name).toBe('anne donne');
+  });
+});
+
+describe('test getLocalScore form user imput', () => {
+  localStorageTest.saveLocalScore(99);
+  const score = localStorageTest.getLocalScore();
+
+  test('should return score 99', () => {
+    expect(score).toBe(99);
   });
 });
